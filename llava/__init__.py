ae261504cd81f5fc02d8d678f787abaddfd1b180
Altan Orhon <altan@uw.edu>
2024-05-04 18:32:20 -0700

    WIP

--- llava/__init__.py
+++ llava/__init__.py
@@ -1,1 +1,10 @@
 from .model import LlavaLlamaForCausalLM
+
+# Fix for:
+#/opt/conda/envs/llava/lib/python3.11/site-packages/huggingface_hub/file_downl
+#oad.py:1132: FutureWarning: `resume_download` is deprecated and will be remov
+#ed in version 1.0.0. Downloads always resume when possible. If you want to fo
+#rce a new download, use `force_download=True`.      
+import warnings
+warnings.simplefilter(action='ignore', module="huggingface_hub", category=FutureWarning)
+
